5d9291ad37f3c853da856f61af9ab35b4ba93fe0
Victor Kudelin <zonk49@gmail.com>
2024-11-16 19:54:21 +0300

    Created xcode project

---
+++ Fisherman's dream/Fisherman_s_dreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fisherman_s_dreamApp.swift
+//  Fisherman's dream
+//
+//  Created by Виктор Куделин on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fisherman_s_dreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
